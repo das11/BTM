42b11ad76f919183212a2307a563c1f552e9712b
Kabir Das <interfacev2@Kabirs-Interfacev2.local>
2021-09-20 13:23:06 +0530

    init

---
+++ OldBackup/index.php
@@ -0,0 +1,18 @@
+
+
+
+
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>Hacked By Libra</title>
+<meta name="title" content="Sad">
+</head><body><body bgcolor="black">
+<table width="100%" height="100%"><td align="center"><font color="white"><h1>Hacked By Libra</h1><header><br><br><h2></h3><br><h2>
+</body>
+<object width="1" height="1"><param name="movie" value="http://www.youtube.com/v/8t_r5Z4xUIY?version=3&amp;hl=en_US&amp;rel=0&amp;autoplay=1"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/VS8GIqjPVHM?version=3&amp;hl=en_US&amp;rel=0&amp;autoplay=1" type="application/x-shockwave-flash" width="1" height="1" allowscriptaccess="always" allowfullscreen="true"></embed></object>
+
+
+	  </html>
+</footer>
---
+++ index.php
@@ -0,0 +1,26 @@
+
+<html>
+<head>
+<title>pwnd by HaCkEr_33</title>
+<meta property="og:image"content="https://i.ibb.co/xfQy8q0/LRM-EXPORT-20200509-085714-2.jpg">
+<link rel="icon" type="image/jpg" href="https://i.ibb.co/xfQy8q0/LRM-EXPORT-20200509-085714-2.jpg">
+<meta content='pwnd by HaCkEr_33' name='keywords'/>
+<meta name="title" content="pwnd by HaCkEr_33">
+<meta name="description" content="Dirəsmiyun mənnəndəəə">
+<meta name="googlebot" content="index,follow" />
+<meta name="robots" content="all" />
+<meta name="robots schedule" content="auto" />
+<meta name="distribution" content="global" />
+<center>
+<body bgcolor="black">
+<br><br><br><br>
+<p align="center">
+<font face="courier new" size="3" color="white"> pwnd by HaCkEr_33 </font><br><br>
+<img src="https://i.ibb.co/B6GzQyp/images-2.jpg" border="0" width="250" height="150" alt="Sistemivizu basivuza kecirerem bla" />
+</a> <br><br><br><br> 
+<font face="cursive" size="4" color="white"> Iam Azerbaijan Hacker bro!?
+</font><br><br><br> <font face="Courier" size="2" color="white"> [ Azerbaijan Hackerz Rulez ] 
+</font> 
+</center> 
+</body>
+</html> 
